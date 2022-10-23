16d2d6464d6d3c1c6d459a244851bed260c95b6f
Steve <stv_x@icloud.com>
2022-10-23 22:42:42 +0800

    Initial Commit

---
+++ QuickSensor/QuickSensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickSensorApp.swift
+//  QuickSensor
+//
+//  Created by 徐嗣苗 on 2022/10/23.
+//
+
+import SwiftUI
+
+@main
+struct QuickSensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
